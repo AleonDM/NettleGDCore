f1e319bda6ac406c153396341742ec74880b31b3
Aluminium <91447283+AluminumFF@users.noreply.github.com>
2022-03-27 19:35:24 +0300

    Add files via upload

---
+++ accounts/registerGJAccount.php
@@ -0,0 +1,37 @@
+<?php
+include "../config/security.php";
+include "../incl/lib/connection.php";
+require_once "../incl/lib/exploitPatch.php";
+
+if(!isset($preactivateAccounts)){
+	$preactivateAccounts = true;
+}
+
+if($_POST["userName"] != ""){
+	//here im getting all the data
+	$userName = ExploitPatch::remove($_POST["userName"]);
+	$password = ExploitPatch::remove($_POST["password"]);
+	$email = ExploitPatch::remove($_POST["email"]);
+	$secret = "";
+	//checking if name is taken
+	$query2 = $db->prepare("SELECT count(*) FROM accounts WHERE userName LIKE :userName");
+	$query2->execute([':userName' => $userName]);
+	$regusrs = $query2->fetchColumn();
+	if ($regusrs > 0) {
+		echo "-2";
+	}else{
+		$passwords = file("common-passwords.txt");
+		foreach($passwords as $passw) {
+			if(trim($passw) == $password) exit("-5"); 
+		}
+		// cleaning non-registered accounts
+		$query = $db->prepare("DELETE FROM accounts WHERE isActive = 0 AND registerDate <= :date");
+		$query->execute([":date" => time()-86400]);
+		$hashpass = password_hash($password, PASSWORD_DEFAULT);
+		$query = $db->prepare("INSERT INTO accounts (userName, password, email, registerDate, isActive)
+		VALUES (:userName, :password, :email, :time, :isActive)");
+		$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':time' => time(), ':isActive' => $preactivateAccounts ? 1 : 0]);
+		echo "1";
+	}
+}
+?>
